821202dd1bfecabbac66725c2f7e6aec5f344b93
vaish0904 <63866589+vaish0904@users.noreply.github.com>
2021-07-09 19:33:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,6 +29,6 @@ app.get("/",function(req,res){
     });
   });
     });
-app.listen(3000,function(){
-  console.log("Server is running on port 3000.");
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server is running.");
 });
